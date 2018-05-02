7c431a4bf75e312ed5f982d34edd1a01be6761b7
Sarun Rattanasiri <midnight_w@gmx.tw>
2018-05-02 14:26:31 +0700

    generate project with bundler

---
+++ lib/ruby_event_store_event_id_mapper.rb
@@ -0,0 +1,5 @@
+require "ruby_event_store_event_id_mapper/version"
+
+module RubyEventStoreEventIdMapper
+  # Your code goes here...
+end
---
+++ lib/ruby_event_store_event_id_mapper/version.rb
@@ -0,0 +1,3 @@
+module RubyEventStoreEventIdMapper
+  VERSION = "0.1.0"
+end
